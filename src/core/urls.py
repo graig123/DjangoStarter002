ceb73e567dfe4efdf706bcd76a9f792b7442a29c
graig123 <graig123@gmail.com>
2023-02-19 14:46:43 +0100

    Update urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app.views import healthcheck_view
+from core.views import healthcheck_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
