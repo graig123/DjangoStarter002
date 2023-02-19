406552d0ede8de9f000de16eda4ea28ebd066510
graig123 <graig123@gmail.com>
2023-02-19 17:09:13 +0100

    Update settings.py

--- src/core/settings.py
+++ src/core/settings.py
@@ -134,11 +134,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / 'core/static/',
-               '/var/www/static/',
-               '/static/'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static'),
+                    os.path.join(BASE_DIR, 'src/core/static')]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 #STATIC_ROOT = os.path.join(BASE_DIR, 'core/static/')
