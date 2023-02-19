370a3a9bfbbb5f66273ef9c8acf2afca87736fd8
graig123 <graig123@gmail.com>
2023-02-19 15:18:12 +0100

    Update views.py

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,15 @@
 import os
-
+from django.template import loader
 from django.http import JsonResponse, HttpRequest
 
-def healthcheck_view(_request: HttpRequest) -> JsonResponse:
-    return JsonResponse({'status':'Love','hostname': os.getenv('HOSTNAME', 'unspecified')})
 
+#def healthcheck_view(_request: HttpRequest) -> JsonResponse:
+#    return JsonResponse({'status':'Love','hostname': os.getenv('HOSTNAME', 'unspecified')})
+
+#@login_required(login_url="/login/")
+def index(request):
+
+    html_template = loader.get_template('index.html')
+    context = {'powerchip' :'Eset - SmartIt Importer,
+           }
+    return HttpResponse(html_template.render(context, request))
